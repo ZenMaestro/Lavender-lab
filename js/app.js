529fce93a32e85fe72f5d39330722c2ccd8bf338
Likhith J <sai437119@gmail.com>
2025-09-24 21:22:20 +0530

    Final Fix: Point frontend to live Render URL

--- js/app.js
+++ js/app.js
@@ -113,7 +113,7 @@ async function getSuggestions(type) {
   exportContainer.innerHTML = "";
 
   try {
-    const response = await fetch("http://localhost:5000/generate", {
+    const response = await fetch("https://lavender-lab1.onrender.com/generate", {
       method: "POST",
       headers: { "Content-Type": "application/json", "Authorization": `Bearer ${token}` },
       body: JSON.stringify({ prompt }),
